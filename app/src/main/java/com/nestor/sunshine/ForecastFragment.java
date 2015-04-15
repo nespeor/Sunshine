2ee0ace106b5564ecb75d5f4d8808bdf51d8874f
nespeor <nestor_serpex@hotmail.com>
2015-04-15 07:31:57 +0200

    android permission internet

--- app/src/main/java/com/nestor/sunshine/ForecastFragment.java
+++ app/src/main/java/com/nestor/sunshine/ForecastFragment.java
@@ -128,6 +128,9 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
+
+                Log.v (LOG_TAG, "Forecast JSON string: " + forecastJsonStr);
+
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
